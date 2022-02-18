ba7d98f4d409921445f7aaa0cce396542a654791
dogukanbayrak6767@gmail.com <dogukanbayrak6767@gmail.com>
2022-02-18 17:03:48 +0300

    Controller updated

--- src/main/java/com/patika/paycore/CreditApplication/controller/CustomerController.java
+++ src/main/java/com/patika/paycore/CreditApplication/controller/CustomerController.java
@@ -1,15 +1,59 @@
 package com.patika.paycore.CreditApplication.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import com.patika.paycore.CreditApplication.model.Customer;
+import com.patika.paycore.CreditApplication.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
+@RequestMapping("/customer")
 public class CustomerController {
+
+
+    @Autowired
+    private CustomerService customerService;
+
 
     @RequestMapping(method= RequestMethod.GET, path= "/hello")
     public String hello() {
         return "denemeler";
     }
+
+    @GetMapping(path = "/all")
+    public List<Customer> getAllCustomers() {
+
+        return customerService.getAllCustomers();
+    }
+    @PostMapping(path= "/get")
+    public Customer getCustomer(@RequestParam Integer id){
+        return  customerService.getCustomer(id);
+    }
+
+    @PostMapping(path = "/add")
+    public boolean addCustomer(@RequestBody Customer customer) {
+        return customerService.addCustomer(customer);
+
+    }
+
+    @PutMapping(value = "/update")
+    public Customer updateCustomer(@RequestBody Customer customer) {
+        return customerService.updateCustomer(customer);
+    }
+
+    @DeleteMapping(value = "/delete/id={id}")
+    public boolean deleteCustomer(@PathVariable Integer id) {
+        return customerService.deleteCustomer(id);
+    }
+
+
+
+
+
+
+
+
 }
