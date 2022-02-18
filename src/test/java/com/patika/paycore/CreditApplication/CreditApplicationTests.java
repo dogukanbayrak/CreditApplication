eb5151f8c579f0c5ebc2ad7e9fef65aa95ab35d5
dogukanbayrak6767@gmail.com <dogukanbayrak6767@gmail.com>
2022-02-18 16:05:17 +0300

    first commit. Start coding

---
+++ src/test/java/com/patika/paycore/CreditApplication/CreditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patika.paycore.CreditApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
